76110a6da65002176952734ed998d6ecd223eb58
Jonathan Wong <jonathanfwong@gmail.com>
2023-08-14 17:43:07 +1000

    stripe checkout

--- seed.js
+++ seed.js
@@ -4,17 +4,18 @@ const Stripe = require('stripe')
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
 
 const createProducts = async () => {
-  for (const product of products) {
-    const stripeProduct = await stripe.products.create({
-      name: product.name,
-      default_price_data: {
-        currency: product.currency,
-        unit_amount_decimal: product.price
-      },
-      images: [product.image]
-    })
-    console.log(stripeProduct.name, ':', stripeProduct.id)
-  }
+  console.log(process.env.STRIPE_SECRET_KEY)
+  // for (const product of products) {
+  //   const stripeProduct = await stripe.products.create({
+  //     name: product.name,
+  //     default_price_data: {
+  //       currency: product.currency,
+  //       unit_amount_decimal: product.price
+  //     },
+  //     images: [product.image]
+  //   })
+  //   console.log(stripeProduct.name, ':', stripeProduct.id)
+  // }
 }
 
 createProducts();
